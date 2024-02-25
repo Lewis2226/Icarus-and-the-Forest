2a9e3c83a5dbe4cd91b4b124b8697362d5572888
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-02-24 23:53:10 -0600

    Versión .12
    -Ya están todos enemigos y funciona lo mínimo que deberían hacer, todavía no pueden morir

--- Assets/Scripts/Enemies/EnemyFollow.cs
+++ Assets/Scripts/Enemies/EnemyFollow.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyFollow : EnemyHazard
 {
     public float velocidadMovimiento;
-    public Transform playerTransform;
-    public bool lookingRight;
+    public Transform playertransform;
+    public float alturainicial;
+    public bool tocandopiso;
+    public Collider2D test;
+    
+
+
+    private void Start()
+    {
+        alturainicial = transform.position.y;
+        test.enabled = false;
+    }
+
+    public void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playertransform.position, velocidadMovimiento * Time.deltaTime);
+        if (playertransform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1);
+        }
+    }
+
+    
 }
