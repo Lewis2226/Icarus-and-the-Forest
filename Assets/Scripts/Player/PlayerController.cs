1f9d2fc0781019445b56d6631658a92a7194fb0a
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-02-27 22:32:37 -0600

    Avance de 27/02/24
    Solo falta el nivel final, el cambio de escenas y los menús

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -16,8 +16,8 @@ public class PlayerController : MonoBehaviour
 
     //Variables doble salto
     public bool dobleJump;
-    
-    
+
+
 
     //Variables salto cargado 
     private float jumpTimerCounter;
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Animator _animator;
     public TrailRenderer trailRenderer;
-    
+
     //Movimiento 
     private Vector2 _movement;
     public bool _facingRight = true;
@@ -64,21 +64,29 @@ public class PlayerController : MonoBehaviour
 
     //Correr
 
+    //Planear
+    float orginalGravity;
+    public bool isPlannig;
+
     //Otras
     public float radius;
 
     private void Awake() //Se toman los componentes necesarios
-                         
+
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        
     }
 
-
+    public void Start()
+    {
+         orginalGravity = _rigidbody.gravityScale;
+    }
 
     void Update()
     {
-       if (isDashing)
+        if (isDashing)
         {
             return;
         }
@@ -89,24 +97,26 @@ public class PlayerController : MonoBehaviour
         if (_isGrounded)
         {
             dobleJump = false;
+            isPlannig = false;
+            _rigidbody.gravityScale = orginalGravity;
             coyoteTimeCounter = coyoteTime;
         }
         else
         {
             coyoteTimeCounter -= Time.deltaTime;
         }
-       
-        
+
+
         //Salto
         if (Input.GetButtonDown("Jump"))
         {
-            if (_isGrounded )
+            if (_isGrounded)
             {
                 Jump();
                 isJumping = true;
                 jumpTimerCounter = jumptime;
             }
-            
+
             //Coyote time
             if (coyoteTimeCounter > 0)
             {
@@ -114,186 +124,215 @@ public class PlayerController : MonoBehaviour
             }
 
             //Doble salto
-            else if (!dobleJump) 
+            else if (!dobleJump)
             {
                 _animator.SetBool("DobleJump", true);
-                dobleJump = true; 
+                dobleJump = true;
                 Jump();
             }
-
-
-            
         }
 
-       
-        
-
-        //Salto Cargado 
-        if (Input.GetButton("Jump") && isJumping == true)
+        //Planear
+        if (Input.GetKey(KeyCode.LeftControl) && !isPlannig)
         {
-            if(jumpTimerCounter > 0)
+            isPlannig = true;
+        }
+        else
+        {
+            isPlannig = false;
+        }
+        if (isPlannig)
+        {
+            
+            Plan();
+        }
+        else
+        {
+            _rigidbody.gravityScale = orginalGravity;
+            isPlannig = false;
+        }
+
+
+
+            //Salto Cargado 
+            if (Input.GetButton("Jump") && isJumping == true)
             {
-                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
-                jumpTimerCounter -= Time.deltaTime;
+                if (jumpTimerCounter > 0)
+                {
+                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+                    jumpTimerCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    isJumping = false;
+                }
             }
-            else
+
+            if (Input.GetButtonUp("Jump"))
             {
                 isJumping = false;
             }
+            //Deslizamiento en pared 
+            WallSlide();
+            //Salto en pared
+            WallJump();
+
+
+
+            if (!isWallJumping)
+            {
+                //Hacer que el personaje gire
+                if (horizontalInput < 0f && _facingRight == true)
+                {
+                    Flip();
+                }
+                else if (horizontalInput > 0f && _facingRight == false)
+                {
+                    Flip();
+                }
+            }
+
+            //Dash
+            if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+            {
+                StartCoroutine(Dash());
+
+            }
+
+
+
+
+            //Revisa si esta en el piso 
+            _isGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, groundLayer);
+
+            //Revisa si esta en pared
+            _onWall = Physics2D.OverlapCircle(wallCheck.position, radius, wallLayer);
         }
 
-        if (Input.GetButtonUp("Jump"))
+        private void FixedUpdate()
         {
-            isJumping = false;
+            //Hacer el movimiento
+            float horizontalVeloctiy = _movement.normalized.x * speed;
+            _rigidbody.velocity = new Vector2(horizontalVeloctiy, _rigidbody.velocity.y);
+
+            if (isDashing)
+            {
+                return;
+            }
         }
-        //Deslizamiento en pared 
-         WallSlide();
+
+        private void LateUpdate()
+        {
+            //Comprobar las animaciones
+            _animator.SetBool("Idle", _movement == Vector2.zero);
+            _animator.SetBool("IsGrounded", _isGrounded);
+            _animator.SetBool("OnWall", _onWall);
+            _animator.SetFloat("VerticalVelocity", _rigidbody.velocity.y);
+            _animator.SetBool("Dash", isDashing);
+            _animator.SetBool("DobleJump", false);
+            _animator.SetBool("Plane", isPlannig);
+        }
+
+        //Hace el giro del personaje
+        private void Flip()
+        {
+            _facingRight = !_facingRight;
+            float localScaleX = transform.localScale.x;
+            localScaleX = localScaleX * -1f;
+            transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.y);
+        }
+
+        //Permite saltar
+        private void Jump()
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+            _isGrounded = false;
+
+        }
+
+        // deslizamiento en pared 
+        private void WallSlide()
+        {
+            if (_onWall && !_isGrounded)
+            {
+                isWallSlide = true;
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, Mathf.Clamp(_rigidbody.velocity.y, -slideSpeed, float.MaxValue));
+            }
+            else
+            {
+                isWallSlide = false;
+            }
+        }
+
         //Salto en pared
-         WallJump();
-        
-        
-
-        if (!isWallJumping)
+        private void WallJump()
         {
-            //Hacer que el personaje gire
-            if (horizontalInput < 0f && _facingRight == true)
+            if (isWallSlide)
             {
-                Flip();
+                isWallJumping = false;
+                wallJumpingDirection = -transform.localScale.x;
+                wallJumpingCouter = wallJumpingTime;
+                CancelInvoke("StopWallJumping");
             }
-            else if (horizontalInput > 0f && _facingRight == false)
+            else
             {
-                Flip();
+                wallJumpingCouter -= Time.deltaTime;
             }
+
+            if (Input.GetButtonDown("Jump") && wallJumpingCouter > 0)
+            {
+                isWallJumping = true;
+                _rigidbody.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+                wallJumpingCouter = 0;
+                dobleJump = false;
+
+
+                if (transform.localScale.x != wallJumpingDirection)
+                {
+                    _facingRight = !_facingRight;
+                    Vector3 localScale = transform.localScale;
+                    localScale.x *= -1;
+                    transform.localScale = localScale;
+                }
+                Invoke("StopWallJumping", wallJumpingDuration);
+
+            }
+        }
+
+        private void StopWallJumping()
+        {
+            isWallJumping = false;
         }
 
         //Dash
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) 
+        private IEnumerator Dash()
         {
-           StartCoroutine(Dash());
-           
+            canDash = false;
+            isDashing = true;
+            _rigidbody.gravityScale = 0;
+            _rigidbody.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
+            trailRenderer.emitting = true;
+            yield return new WaitForSeconds(dashTime);
+            trailRenderer.emitting = false;
+            _rigidbody.gravityScale = orginalGravity;
+            yield return new WaitForSeconds(dashingCooldown);
+            isDashing = false;
+            canDash = true;
         }
 
-        //caida con planeo
-
-        //Revisa si esta en el piso 
-        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, groundLayer);
-
-        //Revisa si esta en pared
-        _onWall = Physics2D.OverlapCircle(wallCheck.position, radius, wallLayer);
-    }
-
-    private void FixedUpdate()
-    {
-        //Hacer el movimiento
-        float horizontalVeloctiy = _movement.normalized.x * speed;
-        _rigidbody.velocity = new Vector2(horizontalVeloctiy, _rigidbody.velocity.y);
-
-        if(isDashing)
-        {
-            return;
-        }
-    }
-
-    private void LateUpdate()
-    {
-        //Comprobar las animaciones
-        _animator.SetBool("Idle", _movement == Vector2.zero);
-        _animator.SetBool("IsGrounded", _isGrounded);
-        _animator.SetBool("OnWall", _onWall);
-        _animator.SetFloat("VerticalVelocity", _rigidbody.velocity.y);
-        _animator.SetBool("Dash", isDashing);
-        _animator.SetBool("DobleJump", false);
-
-
-
-    }
-
-    //Hace el giro del personaje
-    private void Flip()
-    {
-        _facingRight = !_facingRight;
-        float localScaleX = transform.localScale.x;
-        localScaleX = localScaleX * -1f;
-        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.y);
-    }
-
-    //Permite saltar
-    private void Jump()
-    {
-        _rigidbody.velocity = new Vector2 (_rigidbody.velocity.x,jumpForce);
-        _isGrounded = false;
-       
-    }
-
-    // deslizamiento en pared 
-    private void WallSlide()
-    {
-        if (_onWall && !_isGrounded)
-        {
-            isWallSlide = true;
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, Mathf.Clamp(_rigidbody.velocity.y, -slideSpeed, float.MaxValue)); 
-        }
-        else
-        {
-            isWallSlide = false;
-        }
-    }
-
-    //Salto en pared
-    private void WallJump()
-    {
-        if (isWallSlide )
-        {
-            isWallJumping = false;
-            wallJumpingDirection = -transform.localScale.x;
-            wallJumpingCouter = wallJumpingTime;
-            CancelInvoke("StopWallJumping");
-        }
-        else
-        {
-            wallJumpingCouter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump") && wallJumpingCouter > 0)
-        {
-            isWallJumping = true;
-            _rigidbody.velocity = new Vector2( wallJumpingDirection* wallJumpingPower.x , wallJumpingPower.y);
-            wallJumpingCouter = 0;
-            dobleJump = false;
-
-
-            if (transform.localScale.x != wallJumpingDirection)
+        //Planear
+          public void Plan()
+          {
+            isPlannig = true;
+            if(_rigidbody.velocity.y < 0f) 
             {
-                _facingRight = !_facingRight;
-                Vector3 localScale = transform.localScale;
-                localScale.x *= -1;
-                transform.localScale = localScale;
+             _rigidbody.gravityScale = 0.5f;
+            } 
+            else
+            {
+            _rigidbody.gravityScale = orginalGravity;
             }
-            Invoke("StopWallJumping", wallJumpingDuration);
+          }
+    } 
 
-        }
-    }
 
-    private void StopWallJumping()
-    {
-        isWallJumping = false;
-    }
-
-    //Dash
-    private IEnumerator Dash()
-    {
-        canDash = false;
-        isDashing = true;
-        float orginalGravity = _rigidbody.gravityScale;
-        _rigidbody.gravityScale = 0;
-        _rigidbody.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
-        trailRenderer.emitting = true;
-        yield return new WaitForSeconds(dashTime);
-        trailRenderer.emitting = false;
-        _rigidbody.gravityScale = orginalGravity;
-        yield return new WaitForSeconds(dashingCooldown);
-        isDashing = false;
-        canDash = true; 
-    }
- 
-}
